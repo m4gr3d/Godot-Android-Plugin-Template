40217268fdc6798ac7c13f5ba0e581af4516c715
Fredia Huya-Kouadio <fhuya@meta.com>
2023-10-12 06:26:06 -0700

    Update the Godot Android library dependency for beta 1

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
     // TODO: Update the godot dep when 4.2 is stable
-    implementation("org.godotengine:godot:4.2.0.dev-SNAPSHOT")
+    implementation("org.godotengine:godot:4.2.0.beta-SNAPSHOT")
 }
 
 // BUILD TASKS DEFINITION
